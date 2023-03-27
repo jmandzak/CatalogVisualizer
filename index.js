b5759a28e5cc307d4cea518fce2fe72daa70f3d8
Josh Mandzak <jmandzak99@gmail.com>
2023-03-27 14:52:18 -0400

    Abstract prereq line drawing to a different function

--- index.js
+++ index.js
@@ -25,9 +25,6 @@ function catalogClicked(){
     // Clear out whatever is in there currently
     boxSection.innerHTML = "";
 
-    // Clear out any connections
-    $('connection').remove();
-
     // Grab the json and write all the classes
     $.getJSON("scraper/all_catalogs.json", 
     function (data) {
@@ -43,26 +40,38 @@ function catalogClicked(){
             boxSection.innerHTML += '<br>' ;
         }
         
+        // Now draw the prereq lines
+        generatePrereqs();
+    });
+}
+
+function generatePrereqs() {
+    // Clear out any connections
+    $('connection').remove();
+
+    let catalog = $("#dropDownWrite").val()
+
+    $.getJSON("scraper/all_catalogs.json", 
+    function (data) {
 
         // Now let's create the adjacency matrix for this catalog to do prereqs
+        let all_classes = document.getElementsByClassName('box')
         let prereq_matrix = new Array(all_classes.length);
         for(let i = 0; i < prereq_matrix.length; i++) {
             prereq_matrix[i] = new Array(all_classes.length);
         }
 
         const class_regex_string = /[A-Z][A-Z]+ [0-9][0-9]+/g
-        for(let i in all_classes) {
+        for(let i = 0; i < all_classes.length; i++) {
             // Grab the actual class names from inside the string
-            let current_classes = [...all_classes[i].matchAll(class_regex_string)];
+            let current_classes = [...all_classes[i].innerText.matchAll(class_regex_string)];
             if(current_classes.length < 1) {
                 continue;
             }
 
             // Find all of the prereqs for each class listed
             let prereqs = []
-            for(let j in current_classes) {
-                // console.log(data[catalog]['all_courses'][current_classes[j][0]]);
-                // console.log(data[catalog]['all_courses'][current_classes[j][0]]['prereqs']);
+            for(let j = 0; j < current_classes.length; j++) {
                 let temp = []
 
                 // Try catch block here to catch classes that are missing from scraper
@@ -74,14 +83,14 @@ function catalogClicked(){
                 }
 
                 // add all classes from temp array into prereq array
-                for(let k in temp){
+                for(let k = 0; k < temp.length; k++){
                     prereqs.push(temp[k][0])
                 }
             }
 
             // Now check against all of the other classes
             let matches = []
-            for(let j in all_classes) {
+            for(let j = 0; j < all_classes.length; j++) {
                 // don't check the same class
                 if(i == j) {
                     continue;
@@ -89,9 +98,9 @@ function catalogClicked(){
                 
                 // Loop through all prereq possibilities
                 matches = []
-                for(let k in prereqs) {
+                for(let k = 0; k < prereqs.length; k++) {
                     // See if prereq is in class string, if it is then set adj matrix and break
-                    matches = all_classes[j].match(prereqs[k]);
+                    matches = all_classes[j].innerText.match(prereqs[k]);
                     if(matches) {
                         prereq_matrix[i][j] = 1;
                         matches = []
@@ -108,13 +117,14 @@ function catalogClicked(){
             boxes[i].id = "box" + i;
         }
 
-
+        let from_box = "";
+        let box_string = "";
         for(let i = 0; i < prereq_matrix.length; i++) {
-            for(let j = 0; j < prereq_matrix[i].length; j++) {
-                if(prereq_matrix[i][j]) {
-                    let from_box = "#box" + j;
-                    let to_box = "#box" + i;
-                    $(from_box).connections({to: to_box})
+            from_box = '#box' + i;
+            for(let j = 0; j < prereq_matrix.length; j++) {
+                if(prereq_matrix[j][i]) {
+                    let to_box = "#box" + j;
+                    $(from_box).connections({to: to_box});
                 }
             }
         }
